86e175c2a315bbd3edb41b92e3c52f50a8238a49
linusbayere <linusbayere@gmail.com>
2024-10-26 15:53:55 -0400

    add live views with ppr

--- components/view.tsx
+++ components/view.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import Ping from "./ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEW_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 async function View({ id }: { id: string }) {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEW_QUERY, { id });
 
-  // TODO: update the number of views
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
