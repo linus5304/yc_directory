400ca2c9b1a5a101028be486ca34dbe3ff0c6615
linusbayere <linusbayere@gmail.com>
2024-10-25 22:13:17 -0400

    Add search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+import SearchForm from "../../components/search-form";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const { query } = await searchParams;
   return (
     <div>
       <section className="pink_container">
@@ -11,6 +16,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
         </p>
+        <SearchForm query={query} />
       </section>
     </div>
   );
