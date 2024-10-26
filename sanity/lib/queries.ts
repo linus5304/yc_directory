8b83f135e423512eb20adb3c53169d875a99aed4
linusbayere <linusbayere@gmail.com>
2024-10-26 15:01:01 -0400

    enable ppr

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,5 +12,21 @@ export const STARTUPS_QUERY =
   views,
   category,
   description,
-  image
+  image,
+}`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  },
+  views,
+  category,
+  description,
+  image,
+  pitch,
 }`);
