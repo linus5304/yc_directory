fa78955eb22a86c10e3e1d10ec5db37068f6b41b
linusbayere <linusbayere@gmail.com>
2024-10-25 22:52:46 -0400

    Post card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/startup-card";
 import SearchForm from "../../components/search-form";
 
 export default async function Home({
@@ -6,6 +7,22 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const { query } = await searchParams;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Adrain",
+      },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.pexels.com/photos/3912979/pexels-photo-3912979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <div>
       <section className="pink_container">
@@ -17,6 +34,16 @@ export default async function Home({
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.map((post, index) => (
+            <StartupCard key={post._id} post={post} />
+          ))}
+        </ul>
       </section>
     </div>
   );
