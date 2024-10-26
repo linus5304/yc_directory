2a946bae26763fc93ed3b33463384263179cde3a
linusbayere <linusbayere@gmail.com>
2024-10-26 14:32:39 -0400

    add search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,8 +9,9 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const { query } = await searchParams;
+  const params = { search: query || null };
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <div>
